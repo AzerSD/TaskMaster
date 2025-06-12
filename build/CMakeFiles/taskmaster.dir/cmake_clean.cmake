8d4c0e45393e226550d8f5e4b6af86c89f23088e
AzerSD <sioudazer8@gmail.com>
2025-06-12 19:32:01 +0200

    Added a minimal Shell

---
+++ build/CMakeFiles/taskmaster.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/taskmaster.dir/src/config_parser.cpp.o"
+  "CMakeFiles/taskmaster.dir/src/config_parser.cpp.o.d"
+  "CMakeFiles/taskmaster.dir/src/logger.cpp.o"
+  "CMakeFiles/taskmaster.dir/src/logger.cpp.o.d"
+  "CMakeFiles/taskmaster.dir/src/main.cpp.o"
+  "CMakeFiles/taskmaster.dir/src/main.cpp.o.d"
+  "CMakeFiles/taskmaster.dir/src/process_manager.cpp.o"
+  "CMakeFiles/taskmaster.dir/src/process_manager.cpp.o.d"
+  "CMakeFiles/taskmaster.dir/src/shell.cpp.o"
+  "CMakeFiles/taskmaster.dir/src/shell.cpp.o.d"
+  "CMakeFiles/taskmaster.dir/src/taskmaster.cpp.o"
+  "CMakeFiles/taskmaster.dir/src/taskmaster.cpp.o.d"
+  "taskmaster"
+  "taskmaster.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/taskmaster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
